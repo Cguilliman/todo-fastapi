e8ccef941763d7ad6e8c0147cf60b41a5f992c8a
Cguilliman <cguilliman@gmail.com>
2020-04-11 22:18:59 +0300

    Remove member ep

--- rest/routers.py
+++ rest/routers.py
@@ -3,7 +3,7 @@ from typing import *
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from models import Board, get_db, User
+from models import Board, get_db, User, Member
 from contrib.boards import create_board, add_member
 from .schemas import (
     BoardReceive, BoardCreate,
@@ -28,6 +28,12 @@ def board_create(item: BoardCreate, db: Session = Depends(get_db)):
 @router.post("/board/member/add", response_model=MemberAddReceive)
 def board_member_add(item: MemberAdd, db: Session = Depends(get_db)):
     return add_member(db, item)
+
+
+@router.delete("/board/member/{member_id}/delete")
+def board_member_delete(member_id: int, db: Session = Depends(get_db)):
+    db.query(Member).filter(Member.id == member_id).delete()
+    return {"Status": "OK"}
 
 
 @router.post("/user/create")
